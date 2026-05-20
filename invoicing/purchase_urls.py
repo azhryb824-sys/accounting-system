840b6a029e8d0787c9ee3afbadcef2c1b5552524
Abdulrahman <azhryb824@gmail.com>
2026-05-20 22:15:03 +0300

    Add gunicorn and setuptools

--- invoicing/purchase_urls.py
+++ invoicing/purchase_urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<int:id>/edit/', purchase_views.purchase_edit, name='purchase_edit'),
     path('<int:id>/delete/', purchase_views.purchase_delete, name='purchase_delete'),
     path('inventory/', purchase_views.inventory_list, name='inventory_list'),
-    path('inventory/add/', purchase_views.item_add, name='item_add'),
+    #path('inventory/add/', purchase_views.item_add, name='item_add'),
     path('inventory/<int:item_id>/edit/', purchase_views.item_edit, name='item_edit'),
 ]
